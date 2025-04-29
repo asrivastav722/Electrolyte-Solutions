3bcfed23c5c639e86a975c43a2b1cd8412cce641
Akash Raj <asrivastav722@gmail.com>
2025-04-29 05:07:17 -0700

    commit

--- src/Sections/Hero.js
+++ src/Sections/Hero.js
@@ -1,5 +1,6 @@
 
 import { Button } from "antd"
+import Logo from "./../assets/images/Electrolyte.png"
 export default function Hero(){
 
   const openWhatsApp = () => {
@@ -10,11 +11,12 @@ export default function Hero(){
   
     return <header className="ht-65 bg-black w-100 d-flex flex-col align-items-center justify-center">
     <div className="h-fit w-fit m-0 p-0 d-flex flex-col align-items-center gap-2 justify-center">
+      <div className="logo" style={{backgroundImage:`url(${Logo})`}}></div>
       <h1 className="mont text-5xl text-center text-white">Electrolyte Solutions</h1 >
       <p className="poppins text-xl text-white font-extralight">Service at Your Doorstep</p>
-      <Button type="primary" className="bg-black border-slate-100 shadow-none mont p-4 text-base" onClick={() => {openWhatsApp()}}>
+      <button className="colorwhite border-1 border-slate-100 mont px-4 py-3 rounded-xl text-base hover:bg-white" onClick={() => {openWhatsApp()}}>
         Book a Service
-      </Button>
+      </button>
     </div>
   </header>
 }
